294580a778f5f19c89b52ec1fcb1c4dfdf072282
PriyankaBadole <45902143+PriyankaBadole@users.noreply.github.com>
2018-12-17 21:04:13 +0530

    Add files via upload

---
+++ Logical Operators.R
@@ -0,0 +1,34 @@
+#TRUE T
+#FALSE F
+
+4<5
+7>10
+4==5
+
+#>
+# <
+#==
+#<=
+#>=
+#!=
+#!
+#|
+#&
+#istrue(X)
+
+result<- 4<5
+typeof(result)
+
+
+isTRUE(result)
+
+!4>5
+
+result|(4>5)
+
+result&(4>5)
+
+
+
+
+
---
+++ Using Variables.R
@@ -0,0 +1,20 @@
+A<-10L
+typeof(A)
+B<-5
+D<-4
+
+C<-A+D
+typeof(C)
+
+result<- C/A
+
+ans=sqrt(D)
+
+greeting="Hello"
+name="Bob"
+
+message= paste(greeting,name)
+typeof(message)
+
+
+
