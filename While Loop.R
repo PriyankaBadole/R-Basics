5c927ce219f2f0f40033630326b3fe23573b8bb5
PriyankaBadole <45902143+PriyankaBadole@users.noreply.github.com>
2018-12-19 23:11:53 +0530

    Add files via upload

---
+++ While Loop.R
@@ -0,0 +1,18 @@
+while(FALSE){
+  print("Helo")
+}
+
+
+#infinite loop
+while(TRUE){
+  print("Helo")
+}                   
+
+
+#finite loop
+counter=1
+while(counter<11){
+  print(counter)
+  counter<-counter+1
+  
+}
