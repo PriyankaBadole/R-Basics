a389a426a71527b7d62459defade6c3cdb56baa0
PriyankaBadole <45902143+PriyankaBadole@users.noreply.github.com>
2018-12-19 23:10:49 +0530

    Add files via upload

---
+++ Data types in R.R
@@ -0,0 +1,22 @@
+#integer
+X= 2L
+typeof(X)
+
+#Double
+Y= 2.5
+typeof(Y)
+
+#complex
+Z= 2+3i
+typeof(Z)
+
+#character
+a= "h"
+typeof(a)
+
+#logical
+q=T
+typeof(a)
+q=True #TRUE should be in caps
+
+q=TRUE # now it will take as logical
